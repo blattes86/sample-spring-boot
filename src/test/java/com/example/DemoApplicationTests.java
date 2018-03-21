4785bd7d03470e937ff926315532c5e57aff48a2
blattes86 <blattes86@gmail.com>
2018-03-21 16:49:39 +0100

    ajout fonction diviser

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example;
 
-import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,5 +13,30 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+	
+    @Test
+    public void testdivision()
+    {
+        double res=DemoApplication.diviser(8,2);
+        assertEquals(res,4.0,0);
+    }
+    @Test
+    public void testdivision2()
+    {
+        double res=DemoApplication.diviser(12,2);
+        assertEquals(res,6.0,0);
+    }
+    @Test
+    public void testdivision3()
+    {
+        double res=DemoApplication.diviser(12,0);
+        assertEquals(res,0.0,0);
+    }
+    @Ignore
+    @Test
+    public void testdivision4()
+    {
+        double res=DemoApplication.diviser(12,30);
+        assertEquals(res,18.0,0);
+    }
 }
